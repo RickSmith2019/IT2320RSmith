81861c4a2541bbc4a407ceb93661f7586743d41d
RickSmith2019 <S01159091@acad.tri-c.edu>
2019-10-17 15:21:52 -0400

    Late Submission Lab6 Oct 17th.

--- EnrollmentApplication/EnrollmentApplication/Models/Course.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Course.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace EnrollmentApplication.Models
 {
     public class Course
     {
+        [Display(Name ="Course ID")]
         public virtual int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        [Display(Name ="Course Title")]
         public virtual string Title { get; set; }
         public virtual string Description { get; set; }
+        [Required]
+        [Range(1,4)]
+        [Display(Name = "Number of credits")]
         public virtual string Credits { get; set; }
     }
 }
--- EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
+++ EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace EnrollmentApplication.Models
 {
@@ -12,10 +13,18 @@ namespace EnrollmentApplication.Models
         public virtual Student Student { get; set; }
         public virtual int CourseId { get; set; }
         public virtual Course Course { get; set; }
+        [Required]
+        [RegularExpression("A-F")]
         public virtual string Grade { get; set; }
         public virtual bool IsActive { get; set; }
+        [Required]
+        [Display(Name = "Assigned Campus")]
         public virtual string AssignedCampus { get; set; }
+        [Required]
+        [Display(Name = "Enrolled in Semester")]
         public virtual string EnrollmentSemster { get; set; }
+        [Required]
+        [Range(2018,2090)]
         public virtual int EnrollmentYear { get; set; }
     }
 }
